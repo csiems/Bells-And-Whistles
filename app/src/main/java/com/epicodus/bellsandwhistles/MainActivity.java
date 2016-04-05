c9c74cf6a64383c47eb8b8557d2eca68bd95cbac
Christopher Siems and Jeffrey Ruder <Guest@epicodus-72.local>
2016-04-05 11:34:55 -0700

    Implement draggable item

--- app/src/main/java/com/epicodus/bellsandwhistles/MainActivity.java
+++ app/src/main/java/com/epicodus/bellsandwhistles/MainActivity.java
@@ -1,12 +1,21 @@
 package com.epicodus.bellsandwhistles;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.BinderThread;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.CycleInterpolator;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.RelativeLayout.LayoutParams;
 
 import com.epicodus.bellsandwhistles.utils.OnDoubleTapListener;
 
@@ -18,13 +27,73 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
     @Bind(R.id.doubleTapTextView) TextView mDoubleTapTextView;
     @Bind(R.id.longTapTextView) TextView mLongTapTextView;
+    @Bind(R.id.dragTextView) TextView mDragTextView;
+    @Bind(R.id.parentRelativeLayout) RelativeLayout mParentRelativeLayout;
+    @Bind(R.id.outsideRelativeLayout) RelativeLayout mOutsideRelativeLayout;
+
+    private final String msg = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         mLongTapTextView.setOnLongClickListener(this);
+        mDragTextView.setOnLongClickListener(this);
+
+        mOutsideRelativeLayout.setOnDragListener(new View.OnDragListener() {
+
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                return true;
+            }
+        });
+
+        mParentRelativeLayout.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                final View draggedView = (View) event.getLocalState();
+
+                switch(event.getAction())
+                {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
+                        break;
+
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
+                        break;
+
+                    case DragEvent.ACTION_DRAG_EXITED :
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
+                        break;
+
+                    case DragEvent.ACTION_DRAG_LOCATION  :
+                        break;
+
+                    case DragEvent.ACTION_DROP:
+                        Log.d(msg, "ACTION_DROP event");
+                        //getx and gety are getting coords of middle of view
+                        int x_cord = (int) event.getX();
+                        int y_cord = (int) event.getY();
+                        //setx and sety set the upper left corner of view to those coords
+                        //the math below corrects for that
+                        draggedView.setX(x_cord - (draggedView.getWidth()/2));
+                        draggedView.setY(y_cord - (draggedView.getHeight()/2));
+                        draggedView.setVisibility(View.VISIBLE);
+                        break;
+
+                    case DragEvent.ACTION_DRAG_ENDED   :
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
+                        draggedView.setVisibility(View.VISIBLE);
+                        break;
+
+                    default: break;
+                }
+                return true;
+            }
+        });
 
         mDoubleTapTextView.setOnTouchListener(new OnDoubleTapListener(this) {
             @Override
@@ -36,7 +105,8 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
                             .xBy(200)
                             .yBy(200)
                             .setDuration(2000)
-                            .withEndAction(setTextRunnable(mDoubleTapTextView, "Wheee!"));
+                            .withStartAction(setTextRunnable(mDoubleTapTextView, "Wheee!"))
+                            .withEndAction(setTextRunnable(mDoubleTapTextView, "Double Tap Me!"));
                 } else {
                     mDoubleTapTextView.animate()
                             .alpha(1f)
@@ -44,6 +114,7 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
                             .xBy(-200)
                             .yBy(-200)
                             .setDuration(2000)
+                            .withStartAction(setTextRunnable(mDoubleTapTextView, "Wheee!"))
                             .withEndAction(setTextRunnable(mDoubleTapTextView, "Double Tap Me!"));
                 }
             }
@@ -60,8 +131,15 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
                     .setInterpolator(new CycleInterpolator(20))
                     .setDuration(3000)
                     .withStartAction(setTextRunnable(mLongTapTextView, "Earthquake!"))
-                    .withEndAction(setTextRunnable(mLongTapTextView, "Long Press Me!"));
+                    .withEndAction(setTextRunnable(mLongTapTextView, "Long Click Me!"));
         }
+        if (v == mDragTextView) {
+            View.DragShadowBuilder dragShadow = new View.DragShadowBuilder(v);
+            v.startDrag(null, dragShadow, v, 0);
+            v.setVisibility(View.INVISIBLE);
+        }
+
+
         return true;
     }
 
