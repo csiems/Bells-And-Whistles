e446b16396ec9f4a54028694dfe4f121b6051394
Christopher Siems and Jeffrey Ruder <ruderjt@gmail.com>
2016-04-05 13:34:46 -0700

    Finish shake listener

--- app/src/main/java/com/epicodus/bellsandwhistles/MainActivity.java
+++ app/src/main/java/com/epicodus/bellsandwhistles/MainActivity.java
@@ -1,37 +1,48 @@
 package com.epicodus.bellsandwhistles;
 
-import android.graphics.Color;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.annotation.BinderThread;
+
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+
 import android.view.animation.CycleInterpolator;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
+
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.RelativeLayout.LayoutParams;
 
 import com.epicodus.bellsandwhistles.utils.OnDoubleTapListener;
-
-import org.w3c.dom.Text;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener, SensorEventListener {
     @Bind(R.id.doubleTapTextView) TextView mDoubleTapTextView;
     @Bind(R.id.longTapTextView) TextView mLongTapTextView;
     @Bind(R.id.dragTextView) TextView mDragTextView;
     @Bind(R.id.parentRelativeLayout) RelativeLayout mParentRelativeLayout;
     @Bind(R.id.outsideRelativeLayout) RelativeLayout mOutsideRelativeLayout;
+    @Bind(R.id.shakeInstructionsTextView) TextView mShakeInstructionsTextView;
 
     private final String msg = MainActivity.class.getSimpleName();
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 1500;
+    private long lastShakeTime = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,10 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
 
         mLongTapTextView.setOnLongClickListener(this);
         mDragTextView.setOnLongClickListener(this);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mSensor, mSensorManager.SENSOR_DELAY_NORMAL);
 
         mOutsideRelativeLayout.setOnDragListener(new View.OnDragListener() {
 
@@ -138,8 +153,6 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
             v.startDrag(null, dragShadow, v, 0);
             v.setVisibility(View.INVISIBLE);
         }
-
-
         return true;
     }
 
@@ -153,4 +166,46 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         return runnable;
     }
 
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            long currentTime = System.currentTimeMillis();
+            if ((currentTime - lastUpdate) > 100) {
+                long timeDifference = currentTime - lastUpdate;
+                lastUpdate = currentTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/timeDifference * 10000;
+                if (speed > SHAKE_THRESHOLD) {
+                    long now = System.currentTimeMillis();
+                    if (now - lastShakeTime > 1000) {
+                        ColorDrawable background = (ColorDrawable) mOutsideRelativeLayout.getBackground();
+                        if (background.getColor() == getResources().getColor(R.color.colorAccent)) {
+                            mOutsideRelativeLayout.setBackgroundColor(getResources().getColor(R.color.colorGrey));
+                            mShakeInstructionsTextView.setTextColor(getResources().getColor(R.color.colorPrimary));
+                        } else {
+                            mOutsideRelativeLayout.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                            mShakeInstructionsTextView.setTextColor(getResources().getColor(R.color.colorGrey));
+                        }
+                        lastShakeTime = System.currentTimeMillis();
+                    }
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
